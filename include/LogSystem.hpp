778008f0277c0659c6e593b57de597f3f74b87a0
lemon857 <lemon857.an@gmail.com>
2025-02-01 21:24:11 +0500

    fix: refs to functions

--- include/LogSystem.hpp
+++ include/LogSystem.hpp
@@ -56,54 +56,7 @@ public:
   static void Logf(ELogLevel level, std::string&& fstring);
 
   template<typename... _Args>
-	static void Logf(ELogLevel level, std::string&& fstring, _Args &&...args)	{
-  std::vector<std::string> strs;
-
-  (strs.push_back(std::to_string(args)), ...);
-
-#ifdef __LOG_DATETIME__
-  FastPrintDate();
-#endif
-
-  switch (level) {
-   case ELogLevel::kInfo:
-    FastLog("[\033[32minfo\033[39m] ");
-    break;
-  case ELogLevel::kDebug:
-    FastLog("[\033[34mdebug\033[39m] ");
-    break;
-  case ELogLevel::kWarn:
-    FastLog("[\033[33mwarn\033[39m] ");
-    break;
-  case ELogLevel::kError:
-    FastLog("[\033[31merror\033[39m] ");
-    break;
-  case ELogLevel::kCritical:
-    FastLog("[\033[37m\033[41mcritial\033[39m\033[49m] ");
-    break;
-    }
-
-    size_t start = -1;
-    size_t size = fstring.size();
-    for (size_t i = 0; i < size; ++i) {
-      if (fstring[i] == '\\' && (i + 1) < size && fstring[i + 1] == '{') {
-        ++i;
-      } else {
-        if (fstring[i] == '{') {
-          ++i;
-          start = i;
-          while (fstring[i] != '}' && i < size) {
-            ++i;
-          }
-          int j = std::stoi(fstring.substr(start, i));
-          FastLog(strs[j]);
-          continue;
-        }
-      }
-      FastLogChar(fstring[i]);
-    }
-    FastLogChar('\n');
-	}
+	static void Logf(ELogLevel level, std::string&& fstring, _Args &&...args);
 };
 
 namespace std {
@@ -111,4 +64,54 @@ namespace std {
 string to_string(LogSystem::hex number);
 string to_string(string str);
 
+}
+
+template<typename... _Args>
+void LogSystem::Logf(ELogLevel level, std::string&& fstring, _Args &&...args)	{
+std::vector<std::string> strs;
+
+(strs.push_back(std::to_string(args)), ...);
+
+#ifdef __LOG_DATETIME__
+FastPrintDate();
+#endif
+
+switch (level) {
+  case ELogLevel::kInfo:
+  FastLog("[\033[32minfo\033[39m] ");
+  break;
+case ELogLevel::kDebug:
+  FastLog("[\033[34mdebug\033[39m] ");
+  break;
+case ELogLevel::kWarn:
+  FastLog("[\033[33mwarn\033[39m] ");
+  break;
+case ELogLevel::kError:
+  FastLog("[\033[31merror\033[39m] ");
+  break;
+case ELogLevel::kCritical:
+  FastLog("[\033[37m\033[41mcritial\033[39m\033[49m] ");
+  break;
+  }
+
+  size_t start = -1;
+  size_t size = fstring.size();
+  for (size_t i = 0; i < size; ++i) {
+    if (fstring[i] == '\\' && (i + 1) < size && fstring[i + 1] == '{') {
+      ++i;
+    } else {
+      if (fstring[i] == '{') {
+        ++i;
+        start = i;
+        while (fstring[i] != '}' && i < size) {
+          ++i;
+        }
+        int j = std::stoi(fstring.substr(start, i));
+        FastLog(strs[j]);
+        continue;
+      }
+    }
+    FastLogChar(fstring[i]);
+  }
+  FastLogChar('\n');
 }
